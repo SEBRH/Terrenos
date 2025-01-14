3edce0cc41659e66982beea0e00bda43696b34dc
Sebastian Ramirez Hoffmann <u202211894@upc.edu.pe>
2025-01-14 16:11:50 -0500

    Correciones necesarias

--- Terrenos/src/app/app.routes.ts
+++ Terrenos/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component'; // Ajusta la ruta si es necesario
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Página principal
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Manejo de rutas no válidas
+];
