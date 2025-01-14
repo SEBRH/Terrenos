0568d759d9e255a3ab78c05a2d73547f859ce25f
Sebastian Ramirez Hoffmann <u202211894@upc.edu.pe>
2025-01-14 16:14:47 -0500

    routing problem

--- Terrenos/src/app/app.component.ts
+++ Terrenos/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./views/home/home.component";
 import { ToolbarComponent } from "./components/shared/toolbar/toolbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Terrenos/src/app/app.routes.ts
+++ Terrenos/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component'; // Ajusta la ruta si es necesario
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Página principal
-  { path: '**', redirectTo: '', pathMatch: 'full' } // Manejo de rutas no válidas
-];
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige la raíz a /home
+    { path: 'home', component: HomeComponent }, // Ruta para el componente HomeComponent
+    { path: '**', redirectTo: '/home', pathMatch: 'full' } // Manejo de rutas no válidas
+  ];
